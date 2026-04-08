93bdc9b7919a2ff38d7a0b31e5876a0a22ca74ca
Ali Samuel <55445656+phonixerldragonar@users.noreply.github.com>
2026-04-08 11:39:23 +0000

    feat: created introduction_page.dart
    this page only has the setup with on content

---
+++ lib/src/features/introduction/presentation/introduction_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class IntroductionPage extends StatefulWidget {
+  const IntroductionPage({super.key});
+
+  @override
+  State<IntroductionPage> createState() => _IntroductionPageState();
+}
+
+class _IntroductionPageState extends State<IntroductionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
+
